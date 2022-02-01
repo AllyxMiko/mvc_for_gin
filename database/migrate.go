3ab6cf16440197d31e6df9a5615edeeb5001ac3c
allyxmiko <allyxmiko@163.com>
2022-02-01 17:32:11 +0800

    修改数据表迁移方式

--- database/migrate.go
+++ database/migrate.go
@@ -1,13 +1,11 @@
 package libs
 
 import (
-	"mvc_for_gin/models"
-
 	"gorm.io/gorm"
 )
 
 // 在这里进行表迁移
-func DataBaseAutoMirgrate(d *gorm.DB) {
+func DataBaseAutoMirgrate(d *gorm.DB, dst ...interface{}) {
 	// 进行表迁移
-	d.AutoMigrate(&models.UserCD{})
+	d.AutoMigrate(dst...)
 }
