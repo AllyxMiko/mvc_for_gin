0403509082d15bb6fcf2acad4caa26efded18b4d
allyxmiko <allyxmiko@163.com>
2022-02-07 13:16:29 +0800

    空框架

--- router/router.go
+++ router/router.go
@@ -1,14 +1,8 @@
 package router
 
 import (
-	"mvc_for_gin/controller"
-
 	"github.com/gin-gonic/gin"
 )
 
 func RegisterRouter(router *gin.Engine) {
-	router.GET("/", controller.Login)
-	router.GET("/json", controller.JsonIndex)
-	router.GET("/user/:username", controller.FindUser)
-	router.POST("/add", controller.RegisterUser)
 }
